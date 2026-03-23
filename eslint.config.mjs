a2f75e7a4831d0c8cfd8c5fcbb63b7ebc30ba96c
simabui <sima_sima@live.ru>
2026-03-23 12:59:05 +0200

    config stylelint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,6 @@
+import { fixupPluginRules } from "@eslint/compat";
 import js from "@eslint/js";
+import i18next from "eslint-plugin-i18next";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -6,15 +8,16 @@ import tseslint from "typescript-eslint";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
+    plugins: { js, i18next: fixupPluginRules(i18next) },
     extends: ["js/recommended"],
     languageOptions: { globals: { ...globals.browser, __IS_DEV__: "readonly" } },
   },
-  tseslint.configs.recommended,
   {
     rules: {
       "no-unused-vars": 1,
       "no-undef": 1,
+      "i18next/no-literal-string": ["error", { markupOnly: true }],
     },
   },
+  tseslint.configs.recommended,
 ]);
